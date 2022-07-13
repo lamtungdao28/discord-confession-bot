d5e815c3e3c732b6f447a7f926ecfe6a7fd33057
lamtungdao28 <88223391+lamtungdao28@users.noreply.github.com>
2022-07-13 10:22:13 +0700

    Update Bot.ts

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,4 +1,4 @@
 import Client from './Client';
 
 new Client().init();
-require('http').createServer((req:any, res:any) => res.end('Bot is alive!')).listen(process.env.PORT)
+require('http').createServer((req:any, res:any) => res.end('Bot is alive!')).listen(3000)
